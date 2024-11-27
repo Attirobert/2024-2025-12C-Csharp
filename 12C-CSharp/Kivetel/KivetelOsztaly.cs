d957f6033ee6e3c4ac3206bbcdcc87f870ea697f
Attirobert <karobatti@gmail.com>
2024-11-27 10:06:43 +0100

    Kivételkezelés

---
+++ 12C-CSharp/Kivetel/KivetelOsztaly.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kivetel
+{
+    class KivetelOsztaly
+    {
+        public KivetelOsztaly() { }
+
+        // Nincs kivétel kezelés
+        public void KivKezNincs()
+        {
+            Console.WriteLine("Nincs kivételkezelés\nAdjon meg egy karaktert!");
+            int szam = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("A beírt szám: {0}", szam);
+            Console.ReadKey();
+        }
+
+        // Általános kivételkezelés
+        public void KivKezAlt()
+        {
+            Console.WriteLine("\nHiba objektum nélküli kivételkezelés\nAdjon meg egy karaktert!");
+            try
+            {
+                int szam = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("A beírt szám: {0}", szam);
+            }
+            catch
+            {
+                Console.WriteLine("Hibás adat!");
+            }
+            Console.ReadKey();
+        }
+
+        // Őshiba objektummal
+        public void KivKezOs()
+        {
+            Console.WriteLine("\nŐs Hiba objektummal kivételkezelés\nAdjon meg egy karaktert!");
+            try
+            {
+                int szam = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("A beírt szám: {0}", szam);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
