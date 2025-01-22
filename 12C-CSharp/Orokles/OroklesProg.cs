700e93950687caf62571251c6ba31464ae85764a
Attirobert <karobatti@gmail.com>
2025-01-22 10:05:00 +0100

    Orokles projekt kommentezése, rendezése

--- 12C-CSharp/Orokles/OroklesProg.cs
+++ 12C-CSharp/Orokles/OroklesProg.cs
@@ -12,99 +12,98 @@ using System.Threading.Tasks;
  */
 namespace Orokles
 {
+    // Átlagos osztály
+    class Allat
+    {
+        // Properties
+        public string Name { get; set; }
+        public string Hang { get; set; }
+
+        // Konstruktorok
+        public Allat()
+        {
+            this.Name = "Anonim";
+            this.Hang = "Én egy Allat vagyok";
+        }
+
+        public Allat(string name)
+        {
+            this.Name = name;
+            this.Hang = "Én egy Allat vagyok";
+        }
+
+        // Metódusok
+        public void hangotAd()
+        {
+            Console.WriteLine("Ezt a Allat.hangotAd() metódus írta ki.");
+            Console.ReadKey();
+        }
+
+        // Virtuális metódus
+        public virtual void mozog()
+        {
+            Console.WriteLine("Ezt a virtuális mozog() metódus írta ki.");
+            Console.ReadKey();
+        }
+    }
+
+    // ok
+    class Kutya : Allat
+    {
+        public Kutya() : base()
+        {
+            this.Hang = "Én egy kutya vagyok";
+            Console.ReadKey();
+        }
+
+        // Átlagos metódus felülírása (shadow hatás)
+        public void hangotAd()
+        {
+            Console.WriteLine("Ezt a Kutya.hangotAd() metódus írta ki.");
+            Console.ReadKey();
+        }
+
+        // Felül írt virtuális metódus lezárással
+        public sealed override void mozog()
+        {
+            Console.WriteLine("Ezt a lezárt mozog() metódus írta ki.");
+            Console.ReadKey();
+        }
+    }
+
+    class Komondor : Kutya
+    {
+        public Komondor() : base()
+        {
+            this.Name = "Komondor";
+        }
+
+        // Lezárt metódust nem írhatjuk felül
+        //public override void mozog()
+        //{
+        //    Console.WriteLine("Elindultam");
+        //}
+
+        public void mozog(){ }
+    }
+
+    // Osztály lezárása
+    sealed class Macska : Allat
+    {
+        public Macska()
+        {
+            this.Name = "Cicamica";
+        }
+    }
+
+    /*//Lezárt osztályból nem származtatható
+    class Sziami : Macska
+    {
+
+    }*/
+
     internal class OroklesProg
     {
-        // Átlagos osztály
-        class Allat
-        {
-            // Properties
-            public string Name { get; set; }
-            public string Hang { get; set; }
-
-            // Konstruktorok
-            public Allat()
-            {
-                this.Name = "Anonim";
-                this.Hang = "Én egy Allat vagyok";
-            }
-
-            public Allat(string name)
-            {
-                this.Name = name;
-                this.Hang = "Én egy Allat vagyok";
-            }
-
-            // Metódusok
-            public void hangotAd()
-            {
-                Console.WriteLine("Ezt a Allat.hangotAd() metódus írta ki.");
-                Console.ReadKey();
-            }
-
-            // Virtuális metódus
-            public virtual void mozog()
-            {
-                Console.WriteLine("Ezt a virtuális mozog() metódus írta ki.");
-                Console.ReadKey();
-            }
-        }
-
-        // ok
-        class Kutya : Allat
-        {
-            public Kutya() : base()
-            {
-                this.Hang = "Én egy kutya vagyok";
-                Console.ReadKey();
-            }
-
-            // Átlagos metódus felülírása (shadow hatás)
-            public void hangotAd()
-            {
-                Console.WriteLine("Ezt a Kutya.hangotAd() metódus írta ki.");
-                Console.ReadKey();
-            }
-
-            // Felül írt virtuális metódus lezárással
-            public sealed override void mozog()
-            {
-                Console.WriteLine("Ezt a lezárt mozog() metódus írta ki.");
-                Console.ReadKey();
-            }
-        }
-
-        class Komondor : Kutya
-        {
-            public Komondor() : base()
-            {
-                this.Name = "Komondor";
-            }
-
-            // Lezárt metódust nem írhatjuk felül
-            //public override void mozog()
-            //{
-            //    Console.WriteLine("Elindultam");
-            //}
-
-            public void mozog(){ }
-        }
-
-        // Osztály lezárása
-        sealed class Macska : Allat
-        {
-            public Macska()
-            {
-                this.Name = "Cicamica";
-            }
-        }
-
-        /*//Lezárt osztályból nem származtatható
-        class Sziami : Macska
-        {
-
-        }*/
-
-
         static void Main(string[] args)
         {
         }
