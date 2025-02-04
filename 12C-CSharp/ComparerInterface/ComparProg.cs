0f57f53467c79a3560109772c10702b88304bb64
Attirobert <robatti@gmail.com>
2025-02-04 10:58:32 +0100

    IComparer kész

--- 12C-CSharp/ComparerInterface/ComparProg.cs
+++ 12C-CSharp/ComparerInterface/ComparProg.cs
@@ -3,13 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+/*
+ * Objektumok összehasonlítását teszi lehetővé egyedi rendezési logikával.
+Az Compare metódust implementálja.
+Gyakran használják listák vagy tömbök rendezésére egyedi logikával.
+*/
 
 namespace ComparerInterface
 {
-    internal class ComparProg
+    // Minta osztály
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    // Összehasonlítás az ár alapján
+    public class PriceComparer : IComparer<Product>
+    {
+        public int Compare(Product x, Product y)
+        {
+            return x.Price.CompareTo(y.Price);
+        }
+    }
+    class ComparProg
     {
         static void Main(string[] args)
         {
+            List<Product> products = new List<Product>
+        {
+            new Product { Name = "Alma", Price = 10 },
+            new Product { Name = "Barack", Price = 5 },
+            new Product { Name = "Cseresznye", Price = 15 }
+        };
+
+            // A rendezés metódusa a PriceComparer osztály Compare metódusát használja
+            products.Sort(new PriceComparer());
+
+            foreach (var product in products)
+            {
+                Console.WriteLine($"{product.Name}: {product.Price}");
+            }
+
+            Console.ReadKey();
         }
     }
 }
