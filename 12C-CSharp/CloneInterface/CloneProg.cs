04c3e4c5791f8bb000f114e7375555435ea7790a
Attirobert <robatti@gmail.com>
2025-02-04 10:35:16 +0100

    ICloneable kész

---
+++ 12C-CSharp/CloneInterface/CloneProg.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Lehetővé teszi egy objektum másolatának létrehozását.
+Egyetlen metódusa van: Clone.
+Mély vagy sekély másolatot hozhat létre.
+*/
+
+namespace CloneInterface
+{
+    public class Person : ICloneable
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        // Az interfészben deklarált metódus megvalósítása
+        public object Clone()
+        {
+            return new Person { Name = this.Name, Age = this.Age };
+        }
+    }
+
+    internal class CloneProg
+    {
+        static void Main(string[] args)
+        {
+            Person original = new Person { Name = "John", Age = 30 };
+            Person clone = (Person)original.Clone();
+
+            Console.WriteLine($"Eredeti: {original.Name}, {original.Age}");
+            Console.WriteLine($"Másolat: {clone.Name}, {clone.Age}");
+        }
+    }
+}
