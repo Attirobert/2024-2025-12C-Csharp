4a4608b0214cfaf111fcfb2e8f228c32ec0f96bd
Attirobert <karobatti@gmail.com>
2025-02-19 07:46:02 +0100

    Új projektek

---
+++ 12C-CSharp/19.02.TextAlign2/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextAlign2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lblSzoveg_MouseClick(object sender, MouseEventArgs e)
+        {
+            int n = e.X / (lblSzoveg.Width / 3);
+            int m = e.Y / (lblSzoveg.Height / 3);
+            switch(m*3 + n)
+            {
+                case 0: lblSzoveg.TextAlign = ContentAlignment.TopLeft;break;
+                case 1: lblSzoveg.TextAlign = ContentAlignment.TopCenter;break;
+                case 2: lblSzoveg.TextAlign = ContentAlignment.TopRight;break;
+                case 3: lblSzoveg.TextAlign = ContentAlignment.MiddleLeft;break;
+                case 4: lblSzoveg.TextAlign = ContentAlignment.MiddleCenter;break;
+                case 5: lblSzoveg.TextAlign = ContentAlignment.MiddleRight;break;
+                case 6: lblSzoveg.TextAlign = ContentAlignment.BottomLeft;break;
+                case 7: lblSzoveg.TextAlign = ContentAlignment.BottomCenter;break;
+                case 8: lblSzoveg.TextAlign = ContentAlignment.BottomRight;break;
+            }
+        }
+    }
+}
---
+++ 12C-CSharp/19.04.WTeszt/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace WTeszt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnAljara = new System.Windows.Forms.Button();
+            this.btnJobbszelre = new System.Windows.Forms.Button();
+            this.btnJobbra = new System.Windows.Forms.Button();
+            this.btnBalszelre = new System.Windows.Forms.Button();
+            this.btnBalra = new System.Windows.Forms.Button();
+            this.btnKozepre = new System.Windows.Forms.Button();
+            this.btnLe = new System.Windows.Forms.Button();
+            this.btnFel = new System.Windows.Forms.Button();
+            this.btnTetejere = new System.Windows.Forms.Button();
+            this.grbAtlat = new System.Windows.Forms.GroupBox();
+            this.btnAtlatCsok = new System.Windows.Forms.Button();
+            this.btnAtlatNo = new System.Windows.Forms.Button();
+            this.grbMeret = new System.Windows.Forms.GroupBox();
+            this.btnMeretCsokken = new System.Windows.Forms.Button();
+            this.btnMeretNo = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.grbAtlat.SuspendLayout();
+            this.grbMeret.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.Controls.Add(this.btnAljara);
+            this.panel1.Controls.Add(this.btnJobbszelre);
+            this.panel1.Controls.Add(this.btnJobbra);
+            this.panel1.Controls.Add(this.btnBalszelre);
+            this.panel1.Controls.Add(this.btnBalra);
+            this.panel1.Controls.Add(this.btnKozepre);
+            this.panel1.Controls.Add(this.btnLe);
+            this.panel1.Controls.Add(this.btnFel);
+            this.panel1.Controls.Add(this.btnTetejere);
+            this.panel1.Controls.Add(this.grbAtlat);
+            this.panel1.Controls.Add(this.grbMeret);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnAljara
+            // 
+            this.btnAljara.Location = new System.Drawing.Point(301, 325);
+            this.btnAljara.Name = "btnAljara";
+            this.btnAljara.Size = new System.Drawing.Size(75, 23);
+            this.btnAljara.TabIndex = 12;
+            this.btnAljara.Text = "Aljára";
+            this.btnAljara.UseVisualStyleBackColor = true;
+            this.btnAljara.Click += new System.EventHandler(this.btnAljara_Click);
+            // 
+            // btnJobbszelre
+            // 
+            this.btnJobbszelre.Location = new System.Drawing.Point(504, 214);
+            this.btnJobbszelre.Name = "btnJobbszelre";
+            this.btnJobbszelre.Size = new System.Drawing.Size(75, 23);
+            this.btnJobbszelre.TabIndex = 11;
+            this.btnJobbszelre.Text = "Jobbszélre";
+            this.btnJobbszelre.UseVisualStyleBackColor = true;
+            this.btnJobbszelre.Click += new System.EventHandler(this.btnJobbszelre_Click);
+            // 
+            // btnJobbra
+            // 
+            this.btnJobbra.Location = new System.Drawing.Point(399, 214);
+            this.btnJobbra.Name = "btnJobbra";
+            this.btnJobbra.Size = new System.Drawing.Size(75, 23);
+            this.btnJobbra.TabIndex = 10;
+            this.btnJobbra.Text = "Jobbra";
+            this.btnJobbra.UseVisualStyleBackColor = true;
+            this.btnJobbra.Click += new System.EventHandler(this.btnJobbra_Click);
+            // 
+            // btnBalszelre
+            // 
+            this.btnBalszelre.Location = new System.Drawing.Point(99, 214);
+            this.btnBalszelre.Name = "btnBalszelre";
+            this.btnBalszelre.Size = new System.Drawing.Size(75, 23);
+            this.btnBalszelre.TabIndex = 9;
+            this.btnBalszelre.Text = "Balszélre";
+            this.btnBalszelre.UseVisualStyleBackColor = true;
+            this.btnBalszelre.Click += new System.EventHandler(this.btnBalszelre_Click);
+            // 
+            // btnBalra
+            // 
+            this.btnBalra.Location = new System.Drawing.Point(197, 214);
+            this.btnBalra.Name = "btnBalra";
+            this.btnBalra.Size = new System.Drawing.Size(75, 23);
+            this.btnBalra.TabIndex = 8;
+            this.btnBalra.Text = "Balra";
+            this.btnBalra.UseVisualStyleBackColor = true;
+            this.btnBalra.Click += new System.EventHandler(this.btnBalra_Click);
+            // 
+            // btnKozepre
+            // 
+            this.btnKozepre.Location = new System.Drawing.Point(300, 214);
+            this.btnKozepre.Name = "btnKozepre";
+            this.btnKozepre.Size = new System.Drawing.Size(75, 23);
+            this.btnKozepre.TabIndex = 7;
+            this.btnKozepre.Text = "Középre";
+            this.btnKozepre.UseVisualStyleBackColor = true;
+            this.btnKozepre.Click += new System.EventHandler(this.btnKozepre_Click);
+            // 
+            // btnLe
+            // 
+            this.btnLe.Location = new System.Drawing.Point(300, 273);
+            this.btnLe.Name = "btnLe";
+            this.btnLe.Size = new System.Drawing.Size(75, 23);
+            this.btnLe.TabIndex = 6;
+            this.btnLe.Text = "Le";
+            this.btnLe.UseVisualStyleBackColor = true;
+            this.btnLe.Click += new System.EventHandler(this.btnLe_Click);
+            // 
+            // btnFel
+            // 
+            this.btnFel.Location = new System.Drawing.Point(300, 165);
+            this.btnFel.Name = "btnFel";
+            this.btnFel.Size = new System.Drawing.Size(75, 23);
+            this.btnFel.TabIndex = 4;
+            this.btnFel.Text = "Fel";
+            this.btnFel.UseVisualStyleBackColor = true;
+            this.btnFel.Click += new System.EventHandler(this.btnFel_Click);
+            // 
+            // btnTetejere
+            // 
+            this.btnTetejere.Location = new System.Drawing.Point(300, 120);
+            this.btnTetejere.Name = "btnTetejere";
+            this.btnTetejere.Size = new System.Drawing.Size(75, 23);
+            this.btnTetejere.TabIndex = 3;
+            this.btnTetejere.Text = "Tetejére";
+            this.btnTetejere.UseVisualStyleBackColor = true;
+            this.btnTetejere.Click += new System.EventHandler(this.btnTetejere_Click);
+            // 
+            // grbAtlat
+            // 
+            this.grbAtlat.Controls.Add(this.btnAtlatCsok);
+            this.grbAtlat.Controls.Add(this.btnAtlatNo);
+            this.grbAtlat.Location = new System.Drawing.Point(172, 356);
+            this.grbAtlat.Name = "grbAtlat";
+            this.grbAtlat.Size = new System.Drawing.Size(302, 59);
+            this.grbAtlat.TabIndex = 2;
+            this.grbAtlat.TabStop = false;
+            this.grbAtlat.Text = "Átlátszóság";
+            // 
+            // btnAtlatCsok
+            // 
+            this.btnAtlatCsok.Location = new System.Drawing.Point(171, 18);
+            this.btnAtlatCsok.Name = "btnAtlatCsok";
+            this.btnAtlatCsok.Size = new System.Drawing.Size(75, 23);
+            this.btnAtlatCsok.TabIndex = 2;
+            this.btnAtlatCsok.Text = "Csökken";
+            this.btnAtlatCsok.UseVisualStyleBackColor = true;
+            this.btnAtlatCsok.Click += new System.EventHandler(this.btnAtlatCsok_Click);
+            // 
+            // btnAtlatNo
+            // 
+            this.btnAtlatNo.Location = new System.Drawing.Point(72, 19);
+            this.btnAtlatNo.Name = "btnAtlatNo";
+            this.btnAtlatNo.Size = new System.Drawing.Size(75, 23);
+            this.btnAtlatNo.TabIndex = 1;
+            this.btnAtlatNo.Text = "Növekszik";
+            this.btnAtlatNo.UseVisualStyleBackColor = true;
+            this.btnAtlatNo.Click += new System.EventHandler(this.btnAtlatNo_Click);
+            // 
+            // grbMeret
+            // 
+            this.grbMeret.Controls.Add(this.btnMeretCsokken);
+            this.grbMeret.Controls.Add(this.btnMeretNo);
+            this.grbMeret.Location = new System.Drawing.Point(172, 37);
+            this.grbMeret.Name = "grbMeret";
+            this.grbMeret.Size = new System.Drawing.Size(302, 60);
+            this.grbMeret.TabIndex = 0;
+            this.grbMeret.TabStop = false;
+            this.grbMeret.Text = "Méret";
+            // 
+            // btnMeretCsokken
+            // 
+            this.btnMeretCsokken.Location = new System.Drawing.Point(169, 18);
+            this.btnMeretCsokken.Name = "btnMeretCsokken";
+            this.btnMeretCsokken.Size = new System.Drawing.Size(75, 23);
+            this.btnMeretCsokken.TabIndex = 2;
+            this.btnMeretCsokken.Text = "Csökken";
+            this.btnMeretCsokken.UseVisualStyleBackColor = true;
+            this.btnMeretCsokken.Click += new System.EventHandler(this.btnMeretCsokken_Click);
+            // 
+            // btnMeretNo
+            // 
+            this.btnMeretNo.Location = new System.Drawing.Point(70, 19);
+            this.btnMeretNo.Name = "btnMeretNo";
+            this.btnMeretNo.Size = new System.Drawing.Size(75, 23);
+            this.btnMeretNo.TabIndex = 1;
+            this.btnMeretNo.Text = "Növekszik";
+            this.btnMeretNo.UseVisualStyleBackColor = true;
+            this.btnMeretNo.Click += new System.EventHandler(this.btnMeretNo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form manipulációk";
+            this.panel1.ResumeLayout(false);
+            this.grbAtlat.ResumeLayout(false);
+            this.grbMeret.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox grbAtlat;
+        private System.Windows.Forms.Button btnAtlatCsok;
+        private System.Windows.Forms.Button btnAtlatNo;
+        private System.Windows.Forms.GroupBox grbMeret;
+        private System.Windows.Forms.Button btnMeretCsokken;
+        private System.Windows.Forms.Button btnMeretNo;
+        private System.Windows.Forms.Button btnJobbszelre;
+        private System.Windows.Forms.Button btnJobbra;
+        private System.Windows.Forms.Button btnBalszelre;
+        private System.Windows.Forms.Button btnBalra;
+        private System.Windows.Forms.Button btnKozepre;
+        private System.Windows.Forms.Button btnLe;
+        private System.Windows.Forms.Button btnFel;
+        private System.Windows.Forms.Button btnTetejere;
+        private System.Windows.Forms.Button btnAljara;
+    }
+}
+
