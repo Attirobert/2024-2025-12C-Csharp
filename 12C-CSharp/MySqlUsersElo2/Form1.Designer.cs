ce2c37af302db4e8c383e443538e17a0cee7580d
Attirobert <robatti@gmail.com>
2025-03-18 20:12:58 +0100

    MySqlUsersElo2 kész

---
+++ 12C-CSharp/MySqlUsersElo2/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿
+namespace MySqlUsersElo2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxID = new System.Windows.Forms.TextBox();
+            this.tbxNev = new System.Windows.Forms.TextBox();
+            this.tbxJelszo = new System.Windows.Forms.TextBox();
+            this.cbxAdmin = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnConn = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbxID
+            // 
+            this.tbxID.Location = new System.Drawing.Point(48, 35);
+            this.tbxID.Name = "tbxID";
+            this.tbxID.Size = new System.Drawing.Size(100, 20);
+            this.tbxID.TabIndex = 0;
+            // 
+            // tbxNev
+            // 
+            this.tbxNev.Location = new System.Drawing.Point(48, 103);
+            this.tbxNev.Name = "tbxNev";
+            this.tbxNev.Size = new System.Drawing.Size(100, 20);
+            this.tbxNev.TabIndex = 0;
+            // 
+            // tbxJelszo
+            // 
+            this.tbxJelszo.Location = new System.Drawing.Point(48, 171);
+            this.tbxJelszo.Name = "tbxJelszo";
+            this.tbxJelszo.Size = new System.Drawing.Size(100, 20);
+            this.tbxJelszo.TabIndex = 0;
+            // 
+            // cbxAdmin
+            // 
+            this.cbxAdmin.AutoSize = true;
+            this.cbxAdmin.Location = new System.Drawing.Point(48, 231);
+            this.cbxAdmin.Name = "cbxAdmin";
+            this.cbxAdmin.Size = new System.Drawing.Size(61, 17);
+            this.cbxAdmin.TabIndex = 1;
+            this.cbxAdmin.Text = "Admin?";
+            this.cbxAdmin.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Azonosító";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Név";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Jelszó";
+            // 
+            // btnConn
+            // 
+            this.btnConn.Location = new System.Drawing.Point(209, 31);
+            this.btnConn.Name = "btnConn";
+            this.btnConn.Size = new System.Drawing.Size(115, 23);
+            this.btnConn.TabIndex = 5;
+            this.btnConn.Text = "Kapcsolódás";
+            this.btnConn.UseVisualStyleBackColor = true;
+            this.btnConn.Click += new System.EventHandler(this.btnConn_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(209, 88);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(115, 23);
+            this.btnOpen.TabIndex = 5;
+            this.btnOpen.Text = "Megnyitás olvasásra";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(209, 145);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(115, 23);
+            this.btnRead.TabIndex = 5;
+            this.btnRead.Text = "Olvas";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(209, 202);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(115, 23);
+            this.btnInsert.TabIndex = 5;
+            this.btnInsert.Text = "Beszúrás";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(209, 259);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(115, 23);
+            this.btnUpdate.TabIndex = 5;
+            this.btnUpdate.Text = "Módosítás kezdés";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(209, 316);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(115, 23);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Törlés";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(51, 316);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(115, 23);
+            this.btnClose.TabIndex = 5;
+            this.btnClose.Text = "Kapcsolat bontása";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 450);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnConn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbxAdmin);
+            this.Controls.Add(this.tbxJelszo);
+            this.Controls.Add(this.tbxNev);
+            this.Controls.Add(this.tbxID);
+            this.Name = "Form1";
+            this.Text = "MySQL élő kapcsolat demó";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxID;
+        private System.Windows.Forms.TextBox tbxNev;
+        private System.Windows.Forms.TextBox tbxJelszo;
+        private System.Windows.Forms.CheckBox cbxAdmin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnConn;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ 12C-CSharp/MySqlUsersElo2/Form1.cs
@@ -0,0 +1,381 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace MySqlUsersElo2
+{
+    public partial class Form1 : Form
+    {
+        // Változók definiálása
+        #region Kapcsolat properties
+        private MySqlConnection msqlConn;
+        private MySqlDataReader msqlDr;
+        #endregion Kapcsolat properties
+
+        #region Tárolt eljárások
+        private string userTeljesLista = "usersTeljesLista";
+        private string userInsert = "UserInsert";
+        private string userUpdate = "UserUpdate";
+        private string userDelete = "UserDelete";
+        #endregion Tárolt eljárások
+
+        #region A Form és az adatbázis állapotai
+        private enum FormState
+        {
+            Closed, // Zárva, nincs csatlakozva
+            Opened,     // Adatbázishoz csatlakozva, de olvasásra nincs megnyitva
+            Reading,    // Olvasás közben
+            EditInsert,  // Beszúrás adatainak beírása közben
+            EditUpdate  // Rekord szerkesztése közben
+        }
+        private FormState formState = FormState.Closed;
+        #endregion A Form és az adatbázis állapotai
+
+        // Gomb feliratok
+        private string insBasic = "Beszúrás";
+        private string insEdit = "Szerkesztés vége";
+        private string updBasic = "Módosítás";
+        private string updEdit = "Módosítás vége";
+
+        // Üzenet szövegek
+        private string openSikeres = "A kapcsolódás az adatbázishoz sikeres!",
+            openNemSikeres = "A kapcsolódás az adatbázishoz sikertelen!",
+            canToRead = "Az olvasás megkezdődhet!",
+            closedDB = "Az adatbázis bezárva";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConn_Click(object sender, EventArgs e)
+        {
+            mysqlConnect();
+        }
+
+        #region Kapcsolódás az adatbázishoz
+        private void mysqlConnect()
+        {
+            MySqlConnectionStringBuilder sb = new MySqlConnectionStringBuilder();
+            sb.Server = "localhost";
+            sb.UserID = "root";
+            sb.Password = "";
+            sb.Database = "iktat";
+
+            try
+            {
+                msqlConn = new MySqlConnection(sb.ToString());
+                msqlConn.Open();
+                MessageBox.Show(openSikeres);
+
+                formState = FormState.Opened;
+                buttonSwitch(formState);
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show($"{openNemSikeres} \n {ex.Message}");
+            }
+        }
+        #endregion Kapcsolódás az adatbázishoz
+
+        #region Gombok elérhetőségének beállítása
+        private void buttonSwitch(FormState fs)
+        {
+            switch (fs)
+            {
+                case FormState.Closed:
+                    btnConn.Enabled = true;
+                    btnOpen.Enabled = false;
+                    btnRead.Enabled = false;
+                    btnInsert.Enabled = false;
+                    btnUpdate.Enabled = false;
+                    btnDelete.Enabled = false;
+                    btnClose.Enabled = false;
+                    break;
+                case FormState.Opened:
+                    btnConn.Enabled = false;
+                    btnOpen.Enabled = true;
+                    btnRead.Enabled = false;
+                    btnInsert.Enabled = true;
+                    btnUpdate.Enabled = true;
+                    btnDelete.Enabled = true;
+                    btnClose.Enabled = true;
+
+                    tbxID.Enabled = false;
+                    tbxNev.Enabled = false;
+                    tbxJelszo.Enabled = false;
+                    cbxAdmin.Enabled = false;
+
+                    // Gomb feliratok
+                    btnInsert.Text = insBasic;
+                    btnUpdate.Text = updBasic;
+                    break;
+                case FormState.Reading:
+                    btnConn.Enabled = false;
+                    btnOpen.Enabled = false;
+                    btnRead.Enabled = true;
+                    btnInsert.Enabled = true;
+                    btnUpdate.Enabled = true;
+                    btnDelete.Enabled = true;
+                    btnClose.Enabled = true;
+                    break;
+                case FormState.EditInsert:
+                    btnConn.Enabled = false;
+                    btnOpen.Enabled = false;
+                    btnRead.Enabled = false;
+                    btnInsert.Enabled = true;
+                    btnUpdate.Enabled = false;
+                    btnDelete.Enabled = false;
+                    btnClose.Enabled = true;
+
+                    // Beviteli mezők elérhetősége
+                    tbxNev.Enabled = true;
+                    tbxJelszo.Enabled = true;
+                    cbxAdmin.Enabled = true;
+
+                    // Beviteli mezők ürítése
+                    tbxNev.Text = String.Empty;
+                    tbxJelszo.Text = String.Empty;
+                    cbxAdmin.Checked = false;
+
+                    // Gomb feliratok
+                    btnInsert.Text = insEdit;
+                    break;
+                case FormState.EditUpdate:
+                    btnConn.Enabled = false;
+                    btnOpen.Enabled = false;
+                    btnRead.Enabled = false;
+                    btnInsert.Enabled = false;
+                    btnUpdate.Enabled = true;
+                    btnDelete.Enabled = false;
+                    btnClose.Enabled = true;
+
+                    tbxNev.Enabled = true;
+                    tbxJelszo.Enabled = true;
+                    cbxAdmin.Enabled = true;
+
+                    // Gomb feliratok
+                    btnUpdate.Text = updEdit;
+                    break;
+            }
+        }
+        #endregion Gombok elérhetőségének beállítása
+
+        #region Adatbázis megnyitása
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            using (MySqlCommand sqlComm = new MySqlCommand(userTeljesLista, msqlConn))
+            {
+                sqlComm.CommandType = CommandType.StoredProcedure;
+
+                try
+                {
+                    // Olvasás a táblából
+                    msqlDr = sqlComm.ExecuteReader();
+
+                    MessageBox.Show(canToRead);
+
+                    formState = FormState.Reading;
+                    buttonSwitch(formState);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+        #endregion Adatbázis megnyitása
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            NextUser();
+        }
+
+        #region A következő rekord olvasása
+        private void NextUser()
+        {
+            // A következő rekord olvasása
+            msqlDr.Read();
+            tbxID.Text = msqlDr[0].ToString().Trim();
+            tbxNev.Text = msqlDr[1].ToString().Trim();
+            tbxJelszo.Text = msqlDr[2].ToString().Trim();
+            cbxAdmin.Checked = (bool)msqlDr[3];
+        }
+        #endregion A következő rekord olvasása
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            switch (formState)
+            {
+                case FormState.Opened:
+                    formState = FormState.EditInsert;
+                    buttonSwitch(formState);
+                    break;
+                case FormState.Reading:
+                    msqlDr.Close();
+                    formState = FormState.EditInsert;
+                    buttonSwitch(formState);
+                    break;
+                case FormState.EditInsert:
+                    InsertUser(tbxNev.Text, tbxJelszo.Text, (cbxAdmin.Checked) ? 1 : 0);
+                    formState = FormState.Opened;
+                    buttonSwitch(formState);
+                    break;
+            }
+        }
+
+        #region Rekord beszúrása
+        private void InsertUser(string pNev, string pJelszo, int pAdmin)
+        {
+            using (MySqlCommand sqlComm = new MySqlCommand(userInsert, msqlConn))
+            {
+                sqlComm.CommandType = CommandType.StoredProcedure;
+
+                // Paraméterek beállítása
+                MySqlParameter p = new MySqlParameter();
+                p.ParameterName = "pNev";
+                p.Value = pNev;
+                p.MySqlDbType = MySqlDbType.String;
+                sqlComm.Parameters.Add(p);
+
+                sqlComm.Parameters.AddWithValue("pJelszo", pJelszo);
+                sqlComm.Parameters.AddWithValue("pAdmin", pAdmin);
+
+                try
+                {
+                    // Olvasás a táblából
+                    sqlComm.ExecuteNonQuery();
+                    MessageBox.Show("Az rekord felvétele sikeres.");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+        #endregion Rekord beszúrása
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            switch (formState)
+            {
+                case FormState.Opened:
+                    formState = FormState.EditUpdate;
+                    buttonSwitch(formState);
+                    break;
+                case FormState.Reading:
+                    msqlDr.Close();
+                    formState = FormState.EditUpdate;
+                    buttonSwitch(formState);
+                    break;
+                case FormState.EditUpdate:
+                    UpdateUser(Convert.ToInt32(tbxID.Text), tbxNev.Text, tbxJelszo.Text, (cbxAdmin.Checked) ? 1 : 0);
+                    formState = FormState.Opened;
+                    buttonSwitch(formState);
+                    break;
+            }
+        }
+
+        #region Rekord módosítása
+        private void UpdateUser(int pID, string pNev, string pJelszo, int pAdmin)
+        {
+            using (MySqlCommand sqlComm = new MySqlCommand(userUpdate, msqlConn))
+            {
+                sqlComm.CommandType = CommandType.StoredProcedure;
+
+                // Paraméterek beállítása
+                sqlComm.Parameters.AddWithValue("ID", pID);
+
+                MySqlParameter p = new MySqlParameter();
+                p.ParameterName = "Nev";
+                p.Value = pNev;
+                p.MySqlDbType = MySqlDbType.String;
+                sqlComm.Parameters.Add(p);
+
+                sqlComm.Parameters.AddWithValue("Jelszo", pJelszo);
+                sqlComm.Parameters.AddWithValue("Admin", pAdmin);
+
+                try
+                {
+                    // Olvasás a táblából
+                    sqlComm.ExecuteNonQuery();
+                    MessageBox.Show("Az rekord módosítása sikeres.");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+        #endregion Rekord módosítása
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            switch (formState)
+            {
+                case FormState.Reading:
+                    break;
+            }
+            if (formState == FormState.Reading)
+            {
+                msqlDr.Close();
+                formState = FormState.Opened;
+                buttonSwitch(formState);
+            }
+            DeleteUser(Convert.ToInt32(tbxID.Text));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        #region Rekord törlése
+        private void DeleteUser(int pID)
+        {
+            using (MySqlCommand sqlComm = new MySqlCommand(userDelete, msqlConn))
+            {
+                sqlComm.CommandType = CommandType.StoredProcedure;
+
+                // Paraméterek beállítása
+                sqlComm.Parameters.AddWithValue("ID", pID);
+
+                try
+                {
+                    // Törlés a táblából
+                    sqlComm.ExecuteNonQuery();
+                    MessageBox.Show("A rekord törlése sikeres.");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+        #endregion Rekord törlése
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (msqlConn != null) msqlConn.Close();
+            MessageBox.Show(closedDB);
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            msqlConn.Close();
+            MessageBox.Show(closedDB);
+
+            formState = FormState.Closed;
+            buttonSwitch(formState);
+        }
+    }
+}
